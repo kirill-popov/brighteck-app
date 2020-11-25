f26e5a88604d0a0929c63a78a1255ff448defa69
Kirill Popov <kirill@semantria.com>
2020-11-25 16:11:13 +0200

    Minor update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -27,7 +27,7 @@ function TopBar(props) {
 
   return (
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow top-bar">
-      <span class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" >Brightech App</span>
+      <span class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" >React Test App</span>
       {props.loggedIn && <>
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
